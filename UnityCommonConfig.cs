66c725c151dbbecc5781ca95d26e304f4d0a853c
ZeQi_PC <ze.qi@outlook.com>
2025-04-18 20:29:45 +0200

    WIP

---
+++ UnityCommonConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class UnityCommonConfig
+{
+    static UnityCommonConfig()
+    {
+        Debug.Log("Running static");
+    }
+    public static string CommonPacakgePath => "";
+}
